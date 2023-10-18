ac5640ce938bd636a3dad79cf7c5d48f029ccd5a
my computer at home <khanhnd.fg@vietsov.com.vn>
2023-10-18 21:44:50 +0700

    18/10/2023

--- lib/test_screen.dart
+++ lib/test_screen.dart
@@ -10,18 +10,20 @@ class TestScreen extends StatefulWidget {
 }
 
 class _TestScreenState extends State<TestScreen> {
-  List<_SalesData> data = [
-    _SalesData('Jan', 35),
-    _SalesData('Feb', 28),
-    _SalesData('Mar', 34),
-    _SalesData('Apr', 32),
-    _SalesData('May', 40)
-  ];
+  late List<_ChartData> chartData;
+
+  @override
+  void initState() {
+    chartData = getChartData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Syncfusion Flutter chart'),
+          title: const Text('Temperature App'),
+          centerTitle: true,
         ),
         body: Column(children: [
           //Initialize the chart widget
@@ -33,35 +35,40 @@ class _TestScreenState extends State<TestScreen> {
               legend: const Legend(isVisible: true),
               // Enable tooltip
               tooltipBehavior: TooltipBehavior(enable: true),
-              series: <ChartSeries<_SalesData, String>>[
-                LineSeries<_SalesData, String>(
-                    dataSource: data,
-                    xValueMapper: (_SalesData sales, _) => sales.year,
-                    yValueMapper: (_SalesData sales, _) => sales.sales,
-                    name: 'Sales',
+              series: <ChartSeries<_ChartData, int>>[
+                LineSeries<_ChartData, int>(
+                    dataSource: chartData,
+                    xValueMapper: (_ChartData data, _) => data.time,
+                    yValueMapper: (_ChartData data, _) => data.temp_data,
+                    name: 'time',
                     // Enable data label
                     dataLabelSettings: const DataLabelSettings(isVisible: true))
               ]),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              //Initialize the spark charts widget
-              child: SfSparkLineChart.custom(
-                //Enable the trackball
-                trackball: const SparkChartTrackball(
-                    activationMode: SparkChartActivationMode.tap),
-                //Enable marker
-                marker: const SparkChartMarker(
-                    displayMode: SparkChartMarkerDisplayMode.all),
-                //Enable data label
-                labelDisplayMode: SparkChartLabelDisplayMode.all,
-                xValueMapper: (int index) => data[index].year,
-                yValueMapper: (int index) => data[index].sales,
-                dataCount: 5,
-              ),
-            ),
-          )
         ]));
+  }
+
+  List<_ChartData> getChartData() {
+    return <_ChartData>[
+      _ChartData(0, 42),
+      _ChartData(1, 47),
+      _ChartData(2, 33),
+      _ChartData(3, 49),
+      _ChartData(4, 54),
+      _ChartData(5, 41),
+      _ChartData(6, 58),
+      _ChartData(7, 51),
+      _ChartData(8, 98),
+      _ChartData(9, 41),
+      _ChartData(10, 53),
+      _ChartData(11, 72),
+      _ChartData(12, 86),
+      _ChartData(13, 52),
+      _ChartData(14, 94),
+      _ChartData(15, 92),
+      _ChartData(16, 86),
+      _ChartData(17, 72),
+      _ChartData(18, 94),
+    ];
   }
 }
 
@@ -70,4 +77,10 @@ class _SalesData {
 
   final String year;
   final double sales;
+}
+
+class _ChartData {
+  _ChartData(this.time, this.temp_data);
+  final int time;
+  final num temp_data;
 }
